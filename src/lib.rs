affb85efdb50d02fce2a74edc6b4b248887fcf7b
Woomy <woomy@woomy.be>
2022-01-06 13:17:15 +0100

    Mac: Implement from_str

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+use std::{str::FromStr, string::ParseError};
+
+#[derive(Debug, PartialEq)]
+pub enum MacParseError {
+    InvalidInput,
+    InvalidLenght,
+}
+
 pub mod packet;
 #[derive(Debug, PartialEq)]
 pub struct Mac(u8, u8, u8, u8, u8, u8);
@@ -8,6 +16,26 @@ impl Mac {
     }
     pub fn as_bytes(&self) -> [u8; 6] {
         [self.0, self.1, self.2, self.3, self.4, self.5]
+    }
+}
+
+impl FromStr for Mac {
+    type Err = MacParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let bytes = s
+            .split(':')
+            .map(|b| u8::from_str_radix(b, 16))
+            .collect::<Result<Vec<_>, _>>();
+        match bytes {
+            Ok(mac) => {
+                if mac.len() == 6 {
+                    Ok(Mac::new((mac[0], mac[1], mac[2], mac[3], mac[4], mac[5])))
+                } else {
+                    Err(MacParseError::InvalidLenght)
+                }
+            }
+            Err(_) => Err(MacParseError::InvalidInput),
+        }
     }
 }
 
