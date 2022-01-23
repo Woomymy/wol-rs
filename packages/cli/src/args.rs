fe249d02bb676b11300a43c6f784dbbe5f57bfcd
Woomymy <github@woomy.ovh>
2022-01-23 13:59:42 +0100

    CLI: Add CLI interface

---
+++ packages/cli/src/args.rs
@@ -0,0 +1,12 @@
+use argh::FromArgs;
+
+#[derive(FromArgs)]
+/// CLI args
+pub struct Args {
+    #[argh(switch, short = 'v')]
+    /// enable verbose logging
+    pub verbose: bool,
+    /// mac address of host to wake up
+    #[argh(option, short = 'm')]
+    pub mac: String,
+}
